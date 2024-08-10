c78e77e59bf9d95fd381779c21e970c63f1e0afc
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-10 00:01:41 -0700

    Update Board to include more board's information
    Length indicates players' total moves across the board. The method
    empty_at? is updated to differentiate if a position had nil
    (which indicates as an empty spot) or out of bounds.

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -2,12 +2,16 @@
 
 # Manage creation and information of a tic-tac-toe board.
 class Board
+  attr_reader :length
+
   def initialize
     @board = Array.new(9)
+    @length = 0
   end
 
   def update_board(player, position)
     @board[position] = player
+    @length += 1
   end
 
   def display_board
@@ -18,6 +22,34 @@ class Board
   end
 
   def empty_at?(position)
-    !!@board[position]
+    @board.fetch(position, false).nil?
+  end
+
+  def all_horizontal
+    [@board[0, 3], @board[3, 3], @board[6, 3]]
+  end
+
+  def all_vertical
+    vertical = []
+    3.times do |i|
+      temp = []
+      3.times do |j|
+        temp.push(@board[3 * j + i])
+      end
+      vertical.push(temp)
+    end
+    vertical
+  end
+
+  def all_cross
+    cross = []
+    2.times do |i|
+      temp = []
+      3.times do |j|
+        temp.push(@board[i * 2 + j * 4 / (i + 1)])
+      end
+      cross.push(temp)
+    end
+    cross
   end
 end
