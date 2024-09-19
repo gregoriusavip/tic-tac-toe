69eb88d2023213ffbc63a5d084798142308564e6
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-09-18 21:28:43 -0700

    Add tests for update_board

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -2,3 +2,27 @@
 
 require_relative '../lib/game_board'
 require_relative '../lib/player'
+
+describe Board do
+  describe '#update_board' do
+    subject(:empty_board) { described_class.new }
+
+    let(:player_one) { instance_double(Player) }
+
+    context 'when the board is empty' do
+      it 'puts a player object on a given position' do
+        position = 3
+        expect { # rubocop:disable Style/BlockDelimiters
+          empty_board.update_board(player_one, position)
+        }.to change { empty_board.instance_variable_get(:@board)[position] }.from(nil).to(player_one)
+      end
+
+      it 'increments the length of the board' do
+        position = 3
+        expect { # rubocop:disable Style/BlockDelimiters
+          empty_board.update_board(player_one, position)
+        }.to change { empty_board.instance_variable_get(:@length) }.by(1)
+      end
+    end
+  end
+end
